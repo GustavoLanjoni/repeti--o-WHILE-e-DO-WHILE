1bf5b513629ca55cbdbd40f7b33d8854a1f435ff
Gustavo <gustavosilva94514@gmail.com>
2024-02-27 22:22:27 -0300

    first commit

---
+++ src/repetiçãoWhileDoWhile.java
@@ -0,0 +1,27 @@
+public class repetiçãoWhileDoWhile {
+    public static void main(String[] args) throws Exception {
+       
+        int numero = 1;
+
+        /*Estrutura de repetição While*/
+
+        while (numero <= 100000) {
+            
+            System.out.println("O numero atual e: " + numero );
+            numero++;
+
+        }
+        /*------------------------------------------------------------------------ */
+
+            int numero2 = 1;
+            
+
+        do {
+
+            System.out.println("O numero atual e: " + numero2 );
+            numero2++;
+
+
+           } while(numero2 <= 19);
+}
+}
